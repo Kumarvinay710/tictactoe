08f1b1322bfa0528f2fb0afa55f9cfdb28ff971f
Vinay <vin6287183337@gmail.com>
2023-06-01 22:42:05 +0530

    my name

---
+++ script.js
@@ -0,0 +1,76 @@
+console.log("welcome to tic tac toe")
+
+let music= new Audio("news-ting-6832.mp3")
+let winner = new Audio("success-fanfare-trumpets-6185.mp3")
+
+let turn="X";
+let gameover=false;
+
+const changeturn= ()=>{
+    return turn==="X"?"0":"X";
+
+}
+
+const checkwin =()=>{
+    let boxtexts=document.getElementsByClassName('boxtext');
+   let wins=[
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6],
+    
+   ]
+
+   wins.forEach(e=>{
+    if((boxtexts[e[0]].innerText==boxtexts[e[1]].innerText)&&(boxtexts[e[2]].innerText==boxtexts[e[1]].innerText)&&(boxtexts[e[0]].innerText!==""))
+    {  document.querySelector('.info').innerText=boxtexts[e[0]].innerText +" won ";
+      gameover=true;
+      document.querySelector('.imgbox').getElementsByTagName('img')[0].style.width='200px';
+      winner.play();
+   }
+
+   })
+
+}
+
+
+let boxes=document.getElementsByClassName("box");
+Array.from(boxes).forEach(element=>{
+    let boxtext=element.querySelector('.boxtext')
+    element.addEventListener('click',()=>{
+        if(boxtext.innerText===''){
+            boxtext.innerText=turn;
+            turn= changeturn();
+            music.play();
+            checkwin();
+
+            if(!gameover)
+            {
+                document.getElementsByClassName('info')[0].innerText="turn for "+turn;
+        }
+            }
+            
+
+    })
+})
+
+
+reset.addEventListener('click',()=>{
+    let boxtext=document.querySelectorAll('.boxtext')
+    Array.from(boxtext).forEach(element=>{
+        element.innerText=''
+    });
+    turn='X';
+    gameover=false;
+    if(!gameover)
+            {
+                document.getElementsByClassName('info')[0].innerText="turn for "+turn;
+        }
+        document.querySelector('.imgbox').getElementsByTagName('img')[0].style.width='0px';
+            
+            
+})
